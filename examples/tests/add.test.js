4043db6f96bedae817c5414ac84424a3b7735aa2
C5m7b4 <mbedingfield@dcrpos.com>
2021-11-23 21:32:14 -0600

    adds describe to the workflow

--- examples/tests/add.test.js
+++ examples/tests/add.test.js
@@ -1,5 +1,7 @@
 const { add } = require("../src/add");
 
-it("should return 2", () => {
-  expect(add(1, 1)).toEqual(2);
+describe("add", () => {
+  it("should return 2", () => {
+    expect(add(1, 1)).toEqual(2);
+  });
 });
--- tests/one.test.js
+++ tests/one.test.js
@@ -1,5 +1,7 @@
 const { add } = require("../examples/src/add");
 
-it("shoudl add ttwo numbers", () => {
-  expect(add(1, 1)).toEqual(2);
+describe("add", () => {
+  it("shoudl add ttwo numbers", () => {
+    expect(add(1, 1)).toEqual(2);
+  });
 });
