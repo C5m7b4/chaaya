d97b13459957892ff4d07c5608ee2f3b9916d214
C5m7b4 <mbedingfield@dcrpos.com>
2021-11-24 11:58:47 -0600

    add recursive sync search

--- tests/one.test.js
+++ tests/one.test.js
@@ -1,7 +1,13 @@
 const { add } = require("../examples/src/add");
 
 describe("add", () => {
-  it("shoudl add ttwo numbers", () => {
-    expect(add(1, 1)).toEqual(2);
+  it("should equal 2", () => {
+    expect(add(1, 1)).toEqual(4);
+  });
+  it("should equal 3", () => {
+    expect(add(1, 2)).toEqual(5);
+  });
+  it("should be 3", () => {
+    expect(add(1, 2)).toBe(3);
   });
 });
---
+++ util.js
@@ -0,0 +1,61 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const excludeFiles = [
+  ".git",
+  "bin",
+  "node_modules",
+  ".gitignore",
+  "package.json",
+  "package-lock.json",
+];
+const fileExtensions = ["test.js", "spec.js"];
+
+function getExtension(filename) {
+  var i = filename.indexOf(".");
+  return filename.substr(i + 1);
+}
+
+async function findTestfiles(callback) {
+  const dir = __dirname;
+  const testDirectory = path.join(dir, "tests");
+  return await walk(testDirectory);
+}
+
+async function walk(dir) {
+  let files = await fs.readdir(dir);
+  files = await Promise.all(
+    files.map(async (file) => {
+      const filePath = path.join(dir, file);
+      const stats = await fs.stat(filePath);
+      if (stats.isDirectory()) return walk(filePath);
+      else if (stats.isFile()) return filePath;
+    })
+  );
+
+  return files.reduce((all, folderContents) => all.concat(folderContents), []);
+}
+
+// function walk(directory, callback) {
+//   fs.readdir(directory, function (err, files) {
+//     if (err) {
+//       throw new Error(err);
+//     }
+//     files.forEach(function (name) {
+//       if (!excludeFiles.includes(name)) {
+//         var filePath = path.join(directory, name);
+//         var stat = fs.statSync(filePath);
+//         if (stat.isFile()) {
+//           var extension = getExtension(filePath);
+//           if (fileExtensions.includes(extension)) {
+//             testFiles.push(filePath);
+//           }
+//         } else if (stat.isDirectory()) {
+//           walk(filePath, callback);
+//         }
+//       }
+//     });
+//   });
+// }
+
+module.exports = { findTestfiles };
